f97fe55b45257d9cb4fe78760e6feb299d7300fe
ArfiH <arfihussain27@gmail.com>
2026-06-21 08:40:16 +0530

    first commit

---
+++ Animation.h
@@ -0,0 +1,37 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include <vector>
+#include <string>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include "vec2.h"
+
+
+class Animation {
+    static sf::Texture s_defaultTexture; // SFML3: sf::Sprite has no default ctor, so the default Animation() needs a texture to bind to
+    sf::Sprite m_sprite{s_defaultTexture};
+    size_t m_frameCount = 1;       // total number of frames of animation
+    size_t m_currentFrame = 0;     // the current frame of animation being played
+    size_t m_speed = 0;            // the speed to play this animation
+    vec2 m_size = {1, 1}; // size of the animation frame
+    std::string m_name = "none";
+public:
+    Animation();
+
+    Animation(const std::string &name, const sf::Texture &t);
+
+    Animation(std::string name, const sf::Texture &t, size_t frameCount, size_t speed);
+
+    void update();
+
+    bool hasEnded() const;
+
+    const std::string &getName() const;
+
+    const vec2 &getSize() const;
+
+    sf::Sprite &getSprite();
+};
+
+#endif //ANIMATION_H
---
+++ GameEngine.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <utility>
+
+#include "SFML/Window/Event.hpp"
+#include <SFML/Graphics/Image.hpp>
+#include "GameEngine.h"
+#include "Assets.h"
+#include "Scene_Menu.h"
+#include "Scene_Play.h"
+
+
+GameEngine::GameEngine(const std::string &path) {
+    init(path);
+}
+
+void GameEngine::init(const std::string &path) {
+    m_assets.loadFromFile(path);
+
+    m_window.create(sf::VideoMode({1280, 768}), "Definitely Not Mario");
+    m_window.setFramerateLimit(60);
+
+    changeScene("MENU", std::make_shared<Scene_Menu>(this));
+}
+
+std::shared_ptr<Scene> GameEngine::currentScene() {
+    return m_sceneMap[m_currentScene];
+}
+
+bool GameEngine::isRunning() {
+    return m_running & m_window.isOpen(); // maybe '&&' instead of '&'?
+}
+
+sf::RenderWindow &GameEngine::window() {
+    return m_window;
+}
+
+void GameEngine::run() {
+    while (isRunning()) {
+        sUserInput();
+        update();
+        m_window.display();
+    }
+}
+
+void GameEngine::sUserInput() {
+    while (const std::optional event = m_window.pollEvent()) {
+        if (event->is<sf::Event::Closed>()) {
+            quit();
+        }
+
+        if (event->is<sf::Event::KeyPressed>()) {
+            if (event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::X) {
+                std::cout << "Save screenshot to " << "test.png" << std::endl;
+                sf::Texture texture;
+                if (texture.resize(m_window.getSize())) {
+                    std::cerr << "Window resied\n";
+                }
+                texture.update(m_window);
+                if (texture.copyToImage().saveToFile("test.png")) {
+                    std::cout << "Screenshot saved to " << "test.png" << std::endl;
+                }
+            }
+        }
+
+        if (event->is<sf::Event::KeyPressed>() || event->is<sf::Event::KeyReleased>()) {
+            sf::Keyboard::Key keyCode = event->is<sf::Event::KeyPressed>()
+                    ? event->getIf<sf::Event::KeyPressed>()->code
+                    : event->getIf<sf::Event::KeyReleased>()->code;
+
+            // if the current scene does not have an action associated with this key, skip the event
+            if (currentScene()->getActionMap().find(keyCode) == currentScene()->getActionMap().end()) {
+                continue;
+            }
+
+            // determine start or end action by whether it was key press or release
+            const std::string actionType = event->is<sf::Event::KeyPressed>() ? "START" : " END";
+
+            // look up the action and send the action to the scene
+            currentScene()->doAction(Action(currentScene()->getActionMap().at(keyCode), actionType));
+        }
+    }
+}
+
+void GameEngine::changeScene(const std::string &sceneName, std::shared_ptr<Scene> scene, bool endCurrentScene) {
+    m_currentScene = sceneName;
+    m_sceneMap[sceneName] = scene;
+}
+
+void GameEngine::quit() {
+    m_running = false;
+    m_window.close();
+}
+
+void GameEngine::update() {
+    currentScene()->update();
+}
+
+const Assets &GameEngine::assets() const {
+    return m_assets;
+}
